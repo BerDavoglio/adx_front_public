7e9a8ac20cf5a095aeb67382f89895c105ecb1ad
berdavoglio <berdavoglio@gmail.com>
2023-04-19 19:58:29 -0300

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="mb-10">Header</div>
+    <slot />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="text-3xl font-bold underline">Hello World</div> 
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
