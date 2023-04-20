0d672506b42f394b294b01f0eff083d10c69e31c
berdavoglio <berdavoglio@gmail.com>
2023-04-19 23:29:39 -0300

    Create ProductCard component

---
+++ components/ProductCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div
+      class="border-gray-400 hover:bg-gray-200 cursor-pointer border-2 rounded-md w-[24rem] h-[28rem] mx-auto mt-6 p-4">
+      <img :src="obj.img" class="rounded-md">
+      <div class="w-fit m-auto font-semibold text-xl my-2">{{ obj.name }}</div>
+      <div class=" h-[6rem] m-auto text-justify overflow-y-scroll">
+        {{ obj.description }}
+      </div>
+      <div class="mt-2 mx-auto w-fit">R${{ obj.price.toFixed(2) }} | Estoque: {{ obj.stock }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: ['obj'],
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,49 @@
 <template>
   <div>
-    <div>Header</div>
+    <div class="grid py-4 grid-cols-2 m-2">
+      <div class="mx-auto">
+        <div class="text-2xl">
+          <div class="font-semibold">Mini E-Commerce</div>
+        </div>
+        <div class="w-10 m-auto">ADX</div>
+      </div>
+      <div class='grid m-auto cursor-pointer grid-cols-4'>
+        <NuxtLink to="/">
+          <div class="
+          w-24 px-[25%]
+          border-r-2 font-bold
+          hover:bg-gray-300
+          active:bg-gray-400
+          active:text-white
+          py-4
+          border-l-2">
+            Home
+          </div>
+        </NuxtLink>
+        <NuxtLink to="/products">
+          <div class="
+          w-24 px-[15%]
+          border-r-2 font-bold
+          hover:bg-gray-300
+          active:bg-gray-400
+          active:text-white
+          py-4">
+            Produtos
+          </div>
+        </NuxtLink>
+        <NuxtLink to="/cart">
+          <div class="
+          w-24 px-[18%]
+          border-r-2 font-bold
+          hover:bg-gray-300
+          active:bg-gray-400
+          active:text-white
+          py-4">
+            Carrinho
+          </div>
+        </NuxtLink>
+      </div>
+    </div>
     <div class="h-0.5 bg-gray-500 w-[80%] mx-auto mb-4" />
     <slot />
   </div>
